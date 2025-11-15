4f3c61cf39cbcfe5539bebf97c8c838a4ddba675
FatimaIjazOfficial <fatimaijaz.offical@gmail.com>
2025-11-15 21:02:14 +0500

    Add project files.

---
+++ MFT/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace MFT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ssolutionMethod1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ssolutionMethod2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sVersusTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.sVsTanhzsTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ssolutionMethod1ToolStripMenuItem,
+            this.sVsTanhzsTToolStripMenuItem,
+            this.ssolutionMethod2ToolStripMenuItem,
+            this.buttonToolStripMenuItem,
+            this.sVersusTToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3,
+            this.toolStripMenuItem4});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1149, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ssolutionMethod1ToolStripMenuItem
+            // 
+            this.ssolutionMethod1ToolStripMenuItem.BackColor = System.Drawing.Color.Tan;
+            this.ssolutionMethod1ToolStripMenuItem.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ssolutionMethod1ToolStripMenuItem.Name = "ssolutionMethod1ToolStripMenuItem";
+            this.ssolutionMethod1ToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ssolutionMethod1ToolStripMenuItem.Text = "s vs s";
+            this.ssolutionMethod1ToolStripMenuItem.Click += new System.EventHandler(this.ssolutionMethod1ToolStripMenuItem_Click);
+            // 
+            // ssolutionMethod2ToolStripMenuItem
+            // 
+            this.ssolutionMethod2ToolStripMenuItem.BackColor = System.Drawing.Color.DarkOrange;
+            this.ssolutionMethod2ToolStripMenuItem.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold);
+            this.ssolutionMethod2ToolStripMenuItem.Name = "ssolutionMethod2ToolStripMenuItem";
+            this.ssolutionMethod2ToolStripMenuItem.Size = new System.Drawing.Size(135, 20);
+            this.ssolutionMethod2ToolStripMenuItem.Text = "s vs s - tanh(z*s/T)";
+            this.ssolutionMethod2ToolStripMenuItem.Click += new System.EventHandler(this.ssolutionMethod2ToolStripMenuItem_Click);
+            // 
+            // sVersusTToolStripMenuItem
+            // 
+            this.sVersusTToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.sVersusTToolStripMenuItem.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold);
+            this.sVersusTToolStripMenuItem.Name = "sVersusTToolStripMenuItem";
+            this.sVersusTToolStripMenuItem.Size = new System.Drawing.Size(147, 20);
+            this.sVersusTToolStripMenuItem.Text = "s vs T (Fixed Points)";
+            this.sVersusTToolStripMenuItem.Click += new System.EventHandler(this.sVersusTToolStripMenuItem_Click);
+            // 
+            // buttonToolStripMenuItem
+            // 
+            this.buttonToolStripMenuItem.Font = new System.Drawing.Font("News706 BT", 9.75F, System.Drawing.FontStyle.Bold);
+            this.buttonToolStripMenuItem.Name = "buttonToolStripMenuItem";
+            this.buttonToolStripMenuItem.Size = new System.Drawing.Size(286, 20);
+            this.buttonToolStripMenuItem.Text = "Analytical Spontaneous Magnetization s(T)";
+            this.buttonToolStripMenuItem.Click += new System.EventHandler(this.buttonToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(12, 20);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(12, 20);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(12, 20);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RosyBrown;
+            this.button1.Font = new System.Drawing.Font("Verdana", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStripMenuItem4
+            // 
+            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
+            this.toolStripMenuItem4.Size = new System.Drawing.Size(12, 20);
+            // 
+            // sVsTanhzsTToolStripMenuItem
+            // 
+            this.sVsTanhzsTToolStripMenuItem.Name = "sVsTanhzsTToolStripMenuItem";
+            this.sVsTanhzsTToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
+            this.sVsTanhzsTToolStripMenuItem.Text = " s vs tanh(zs/T)";
+            this.sVsTanhzsTToolStripMenuItem.Click += new System.EventHandler(this.sVsTanhzsTToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1149, 875);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ssolutionMethod1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ssolutionMethod2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sVersusTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem buttonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripMenuItem sVsTanhzsTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem4;
+    }
+}
+
---
+++ MFT/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MFT
+{
+    public partial class Form1 : Form
+    {
+        Graphics gg;
+        double z = 4; // coordination number
+        double T;     // temperature, dynamic input
+
+        public Form1()
+        {
+            InitializeComponent();
+            gg = this.CreateGraphics();
+        }
+
+        // Draw axes helper
+        private void DrawAxes(Point origin, string xs, string ys)
+        {
+            Point px1 = new Point(origin.X - 100, origin.Y);
+            Point px2 = new Point(origin.X + 100, origin.Y);
+            Point py1 = new Point(origin.X, origin.Y - 100);
+            Point py2 = new Point(origin.X, origin.Y + 100);
+
+            Pen p = new Pen(Color.Red);
+            SolidBrush bo = new SolidBrush(Color.OrangeRed);
+
+            gg.DrawLine(p, px1, px2);
+            gg.DrawLine(p, py1, py2);
+
+            Font f = new Font("Times New Roman", 12);
+            gg.DrawString(xs, f, bo, origin.X + 100, origin.Y);
+            gg.DrawString(ys, f, bo, origin.X, origin.Y - 100);
+        }
+
+        // General plot helper
+        private void PlotFunction(Func<float, float> func, Point origin, float scaleX, float scaleY, Color color, bool drawAxes = true, string xs = "", string ys = "")
+        {
+            if (drawAxes) DrawAxes(origin, xs, ys);
+
+            using (SolidBrush plotBrush = new SolidBrush(color))
+            {
+                for (float x = -1; x <= 1; x += 0.01f)
+                {
+                    float y = func(x);
+                    gg.FillEllipse(plotBrush, origin.X + x * scaleX, origin.Y - y * scaleY, 6, 6);
+                }
+            }
+        }
+
+        // Get temperature helper
+        private void GetTemperature()
+        {
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Enter temperature T:", "Temperature Input", "2.5");
+            if (!double.TryParse(input, out T))
+            {
+                MessageBox.Show("Invalid input. Using default T = 2.5");
+                T = 2.5;
+            }
+        }
+
+        // 1. s vs s
+        // Purpose: Shows simple identity relation, baseline comparison for MFT
+        private void ssolutionMethod1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PlotFunction(s => s, new Point(150, 220), 120, 120, Color.Red, true, "s", "s");
+        }
+
+        // 2. s vs tanh(z*s/T)
+        // Purpose: Visualize the Mean-Field equation s = tanh(z*s/T)
+        private void sVsTanhzsTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetTemperature();
+            PlotFunction(s => (float)Math.Tanh(z * s / T), new Point(450, 220), 120, 120, Color.Blue, true, "s", "tanh(z*s/T)");
+        }
+
+        // 3. s vs s - tanh(z*s/T)
+        // Purpose: Visualize fixed points graphically
+        // Fixed points occur where s - tanh(z*s/T) = 0
+        private void ssolutionMethod2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetTemperature();
+            PlotFunction(s => s - (float)Math.Tanh(z * s / T), new Point(750, 220), 80, 120, Color.Green, true, "s", "s - tanh(z*s/T)");
+        }
+
+        // 4. Analytical spontaneous magnetization s(T)
+        // Purpose: Shows analytical solution for MFT Ising model
+        // Only valid for T < 4 (critical temperature)
+        private void buttonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Point origin = new Point(120, 650);
+            DrawAxes(origin, "T", "s");
+
+            for (double temp = 0.1; temp < 4; temp += 0.05)
+            {
+                float ans2 = (float)Math.Sqrt(4 - temp);
+                float ans3 = -(float)Math.Sqrt(4 - temp);
+
+                gg.FillEllipse(new SolidBrush(Color.Orange), origin.X + (float)temp * 80, origin.Y - ans2 * 120, 6, 6);
+                gg.FillEllipse(new SolidBrush(Color.Orange), origin.X + (float)temp * 80, origin.Y - ans3 * 120, 6, 6);
+            }
+        }
+
+        // 5. s vs T (Fixed Points)
+        // Purpose: Shows temperature dependence of MFT fixed points
+        // Points where s = tanh(z*s/T)
+        private void sVersusTToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Point origin = new Point(600, 650);
+            DrawAxes(origin, "T", "s");
+
+            for (double temp = 0.1; temp < 10; temp += 0.05)
+            {
+                for (float s = -1; s < 1; s += 0.01f)
+                {
+                    float ans = s - (float)Math.Tanh(z * s / temp);
+                    if (Math.Abs(ans) < 0.001)
+                    {
+                        gg.FillEllipse(new SolidBrush(Color.Purple), origin.X + (float)temp * 40, origin.Y - s * 120, 6, 6);
+                    }
+                }
+            }
+        }
+
+        // Clear screen
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gg.Clear(this.BackColor);
+        }
+    }
+}
